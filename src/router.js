869ed67f693610dc54293220e0a68357ed5ad111
thesolider76 <51596481+thesolider76@users.noreply.github.com>
2020-03-13 15:39:08 +0800

    new

--- src/router.js
+++ src/router.js
@@ -4,16 +4,20 @@ import music from './views/music.vue'
 import songDetail from './views/music-child/song-detail.vue'
 import myCollection from './views/music-child/my-collection.vue'
 import search from './views/music-child/search.vue'
+import explore from './views/music-child/explore.vue'
 import myPlayList from './views/music-child/my-playlist.vue'
-import playListDetail from './views/music-child/playlist-child/playlist-detail.vue'
+import playListDetail from './views/music-child/playlist-detail.vue'
+import albumDetail from './views/music-child/album-detail.vue'
+import artistDetail from './views/music-child/artist-detail.vue'
 
 Vue.use(Router)
 
 var router = new Router({
+    mode: 'history',
     routes:[
         { 
             path:'/',
-            redirect:'/music/my-collection'
+            redirect:'/music/artist-detail'
         },
         {
             path:'/music',
@@ -22,6 +26,11 @@ var router = new Router({
                 keepAlive: false, //此组件不需要被缓存
             },
             children:[
+                {
+                    path: 'explore',
+                    name: 'explore',
+                    component: explore,
+                },
                 {
                     path:'song-detail',
                     name:'song-detail',
@@ -42,29 +51,36 @@ var router = new Router({
                     name:"search",
                     component:search,
                     meta: {
-                        keepAlive: false, //此组件不需要被缓存
+                        keepAlive: true, //此组件不需要被缓存
                     }
                 },
                 {
                     path:'my-playlist',
+                    name: 'my-playlist',
                     component:myPlayList,
-               
-                    children:[
-                        {
-                            path: 'playlist-detail',
-                            name: 'playlist-detail',
-                            component:playListDetail,
-                            meta: {
-                                keepAlive: false,
-                            }
-                        }
-                    ]
+                },
+                {
+                    path: 'playlist-detail',
+                    name: 'playlist-detail',
+                    component: playListDetail,
+                    meta: {
+                        keepAlive: true,
+                        ifDoFresh: false
+                    }
+                },
+                {
+                    path:'album-detail',
+                    name: 'album-detail',
+                    component: albumDetail,
+                },
+                {
+                    path:'artist-detail',
+                    name: 'artist-detail',
+                    component: artistDetail,
                 }
             ]
         },
-
     ],
-    
 })
 
 export default router
