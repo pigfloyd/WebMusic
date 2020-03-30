6eac90ffb7e49acff89b668db509ad63d72669ca
thesolider76 <51596481+thesolider76@users.noreply.github.com>
2020-03-30 11:44:58 +0800

    update

--- src/components/com-login.vue
+++ src/components/com-login.vue
@@ -1,97 +1,254 @@
 <template>
     <div class="overlay">
-         <div class="header">
-                <span>登录账号</span>
-                <button class="close-btn" @click="close">
-                    <i class="fa fa-times" aria-hidden="true"></i> 
-                </button>
-            </div>
         <div class="login-bd">
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="输入手机号" v-model="phoneNum">
+            <button class="close-btn" @click="close">
+                    <i class="fa fa-times" aria-hidden="true"></i> 
+            </button>
+            <div class="header">
+                <span :class="[flag ? 'active' : '']" @click="switchLogin">登录</span>
+                <span :class="[flag ? '' : 'active']" @click="switchRegister">注册</span>
             </div>
-            <div class="input-group mb-3" >
-                <input type="password" class="form-control" placeholder="输入密码" v-model="password">
+            <transition name="fade">
+                <div class="fail-tip" v-show="regiFail" v-text="toast">
+                </div>  
+            </transition>
+            <div class="content">
+                <el-form :model="loginForm" status-icon :rules="rules" ref="loginForm" label-width="100px" style="padding-right: 40px;  padding-top: 20px" class="demo-ruleForm" v-show="flag">
+                    <el-form-item label="用户名" >
+                        <el-input v-model="loginForm.username"></el-input>
+                    </el-form-item>
+                    <el-form-item label="密码">
+                        <el-input type="password" v-model="loginForm.password" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="login">登录</el-button>
+                    </el-form-item>
+                </el-form>
+                <el-form :model="regiForm" status-icon :rules="rules" ref="regiForm" label-width="100px" style="padding-right: 40px;" class="demo-regiForm" v-show="!flag">
+                    <el-form-item label="用户名" prop="username">
+                        <el-input v-model="regiForm.username"></el-input>
+                    </el-form-item>
+                    <el-form-item label="密码" prop="pass">
+                        <el-input type="password" v-model="regiForm.pass" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="确认密码" prop="checkPass">
+                        <el-input type="password" v-model="regiForm.checkPass" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="register">注册</el-button>
+                        <el-button @click="resetForm('regiForm')">重置</el-button>
+                    </el-form-item>
+                </el-form>
             </div>
-            <button type="button" class="commit-btn" @click="login">登录</button>
         </div>
     </div>
 </template>
 <script>
 export default {
     data() {
+        var checkUsername = (rule, value, callback) => {
+            if (!value) {
+            return callback(new Error('用户名不能为空'))
+            }
+            else {
+                callback()
+            }
+        };
+        var validatePass = (rule, value, callback) => {
+            if (value === '') {
+            callback(new Error('请输入密码'))
+            } else {
+            if (this.regiForm.checkPass !== '') {
+                this.$refs.regiForm.validateField('checkPass');
+            }
+            callback()
+            }
+        };
+        var validatePass2 = (rule, value, callback) => {
+            if (value === '') {
+            callback(new Error('请再次输入密码'))
+            } else if (value !== this.regiForm.pass) {
+            callback(new Error('两次输入密码不一致!'))
+            } else {
+            callback()
+            }
+        };
         return {
-            phoneNum: '',
-            password: ''
+            flag: true,
+            loginForm: {
+                password: '',
+                username: ''
+            },
+            regiForm: {
+                pass: '',
+                checkPass: '',
+                username: ''
+            },
+            rules: {
+                pass: [
+                    { validator: validatePass, trigger: 'blur' }
+                ],
+                checkPass: [
+                    { validator: validatePass2, trigger: 'blur' }
+                ],
+                username: [
+                    { validator: checkUsername, trigger: 'blur' }
+                ]
+            },
+            regiFail: false,
+            toast: '',
         }
+    },
+    mounted() {
     },
     methods:{
         close(){
+            this.$refs['loginForm'].resetFields()
+            this.$refs['regiForm'].resetFields()
             this.$emit('close')
         },
-        login() {
-            this.$axios.post(`/login/cellphone?phone=${this.phoneNum}&password=${this.password}`)
+        switchLogin() {
+            this.flag = true
+        },
+        switchRegister() {
+            this.flag = false
+        },
+        login(){
+            this.$refs['loginForm'].validate((valid) => {
+                if (valid) {
+                    let params = new URLSearchParams();
+                    params.append('username', this.loginForm.username)
+                    params.append('password', this.loginForm.password)
+                    this.$axios.post('/api/server/login.php', params)
+                    .then(res => {
+                        if(res.data.logintype === 1){
+                            this.$refs['loginForm'].resetFields();
+                            localStorage.setItem('token', JSON.stringify(res.data.token))
+                            localStorage.setItem('username', JSON.stringify(res.data.account.userName))
+                            console.log(localStorage.getItem('username'))
+                            this.$emit('login', res.data.account.userName)
+                        } else {
+                            this.toast = '用户名或密码错误，登录失败！'
+                            this.regiFail = true
+                            setTimeout(() => {
+                                this.regiFail = false
+                            }, 2500)
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+                } else {
+                    return false;
+                }
+            });
+            this.$refs['loginForm'].resetFields();
+        },
+        register() {
+            this.$refs['regiForm'].validate((valid) => {
+                if (valid) {
+                    let params = new URLSearchParams();
+                    params.append('username', this.regiForm.username)
+                    params.append('password', this.regiForm.pass)
+                    this.$axios.post('/api/server/register.php', params)
+                    .then(res => {
+                        console.log(res.data)
+                        if(res.data.status === 2){
+                            this.$refs['regiForm'].resetFields();
+                            return this.$axios.post('/api/server/login.php', params)
+                        } else {
+                            this.toast = '用户名已注册，注册失败！'
+                            this.regiFail = true
+                            setTimeout(() => {
+                                this.regiFail = false
+                            }, 2500)
+                        }
+                    })
+                    .then(res => {
+                        console.log(res.data)
+                        localStorage.setItem('token', JSON.stringify(res.data.token))
+                        localStorage.setItem('username', JSON.stringify(res.data.account.userName))
+                        this.$emit('login', res.data.account.userName)
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+                } else {
+                    return false
+                }
+            });
+        },
+        resetForm(formName) {
+            this.$refs[formName].resetFields()
         }
-    }
+    },
+
 }
 </script>
 <style scoped>
     .overlay{
         height: 100%;
         width: 100%;
-        background-color: rgba(0, 0, 0, .5);
-        border-radius: 8px;
+        background-color: rgba(0, 0, 0, .3);
+    }
+    .login-bd {
+        width: 400px;
+        height: 340px;
+        position: relative;
+        top: 50%;
+        left: 50%;
+        margin-left: -200px;
+        margin-top: -220px;
+        box-shadow: 0 30px 45px -20px rgba(0, 0, 0, .5);
+        z-index: 999;
     }
     .header{
-        position: absolute;
-        left:50%;
-        transform: translate(-50%,140%);
         height:60px;
         width:400px;
         border-radius: 12px 12px 0 0; 
-        background-color: rgb(226, 220, 220);
-        box-shadow: 0 1px 15px  rgba(0, 0, 0, .5);
-        z-index: 2;
+        background-image: linear-gradient(rgb(30, 144, 255), rgb(5, 119, 230));
+        z-index: 997;
+        display: flex;
     }
-    .login-bd{
-        position: absolute;
-        left:50%;
-        transform: translate(-50%,60%);
-        height:240px;
+    .content{
+        padding-top: 10px;
+        height:280px;
         width:400px;
         padding:20px;
         text-align: center;
         border-radius: 0 0 12px 12px; 
-        box-shadow: 0 3px 15px  rgba(0, 0, 0, .5);
         background-color: #fff;
-        z-index: 1;
+        z-index: 990;
     }
     .header span{
+        color: #fff;
+        cursor: pointer;
+        line-height: 60px;
+        flex: 1;
         font-size:18px;
         font-weight: bold;
-        margin-left: 160px; 
+        text-align: center;
+    }
+    .active {
+        border-bottom: 3px solid white;
     }
     .form-control{
         font-weight: bold;
         box-shadow: inset 0 0 3px rgba(0, 0, 0, .1);
     }
     .close-btn{
-        width:35px;
+        position: absolute;
+        right: -6px;
+        top: -6px;
+        width:30px;
         height:30px;
-        border-radius: 4px;
+        border-radius: 80%;
         outline: none;
         border: none;
         background-color: rgb(246, 103, 103);
         box-shadow: 0 1px 2px 1px  rgba(0, 0, 0, .19);
         color: white;
-        margin-left: 105px; 
-        margin-top: 15px; 
-    }
-    .close-btn:hover{
-        background-image: linear-gradient(rgb(246, 103, 103), rgba(216, 71, 71));
-    }
-    .close-btn:active{
-        background-image: linear-gradient(rgba(216, 71, 71), rgb(246, 103, 103));
+        z-index: 999;
     }
     .commit-btn{
         width:130px;
@@ -103,7 +260,7 @@ export default {
         color: white;
         border: none;
         border-radius: 4px;
-        background-image: linear-gradient(rgb(66, 184, 131), rgba(36, 154, 101));
+        background-color: rgb(66, 184, 131);
         box-shadow: 0 1px 2px 1px  rgba(0, 0, 0, .19), inset 0 1px 0 rgba(255, 255, 255, .4);
         vertical-align: middle;
         line-height: 1px;
@@ -113,5 +270,25 @@ export default {
     .commit-btn:active,.step-btn:active{
         background-image: linear-gradient(rgba(36, 154, 101),rgb(66, 184, 131));
         box-shadow: 0 1px 2px 1px  rgba(0, 0, 0, .19), inset 0 1px 0 rgba(255, 255, 255, .4);
+    }
+    .fail-tip {
+        font-weight: bold;
+        position: absolute;
+        top: 0;
+        height:60px;
+        line-height: 60px;
+        width:400px;
+        border-radius: 12px 12px 0 0; 
+        color: white;
+        text-align: center;
+        background-color: rgb(246, 103, 103);
+        background-image: linear-gradient(rgb(246, 103, 103), rgb(221, 78, 78));
+        z-index: 998;
+    }
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity 0.4s;
+    }
+    .fade-enter, .fade-leave-to {
+        opacity: 0;
     }
 </style>
---
+++ src/components/com-user.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="overlay">
+        <div class="user-bd">
+            <button class="close-btn" @click="close">
+                    <i class="fa fa-times" aria-hidden="true"></i> 
+            </button>
+            <div class="header">
+                <span>用户：{{ username }}</span>
+            </div>
+            <div class="content">
+                <div class="logout-btn" @click="logout">登出</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['username'],
+    data() {
+        
+    },
+    mounted() {
+    },
+    methods:{
+        close(){
+            this.$emit('close')
+        },
+        logout(){
+            this.$emit('logout')
+        }
+    },
+
+}
+</script>
+<style scoped>
+    .overlay{
+        height: 100%;
+        width: 100%;
+        background-color: rgba(0, 0, 0, .3);
+    }
+    .user-bd {
+        width: 400px;
+        height: 200px;
+        position: relative;
+        top: 50%;
+        left: 50%;
+        margin-left: -200px;
+        margin-top: -220px;
+        box-shadow: 0 30px 45px -20px rgba(0, 0, 0, .5);
+        z-index: 999;
+    }
+    .header{
+        height:60px;
+        width:400px;
+        border-radius: 12px 12px 0 0; 
+        background-image: linear-gradient(rgb(30, 144, 255), rgb(5, 119, 230));
+        z-index: 997;
+        display: flex;
+    }
+    .content{
+        padding-top: 10px;
+        height:140px;
+        width:400px;
+        padding:20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 0 0 12px 12px; 
+        background-color: #fff;
+        z-index: 990;
+    }
+    .header span{
+        color: #fff;
+        cursor: pointer;
+        line-height: 60px;
+        flex: 1;
+        font-size:18px;
+        font-weight: bold;
+        text-align: center;
+    }
+    .close-btn{
+        position: absolute;
+        right: -6px;
+        top: -6px;
+        width:30px;
+        height:30px;
+        border-radius: 80%;
+        outline: none;
+        border: none;
+        background-color: rgb(246, 103, 103);
+        box-shadow: 0 1px 2px 1px  rgba(0, 0, 0, .19);
+        color: white;
+        z-index: 999;
+    }
+    .logout-btn {
+        text-align: center;
+        cursor: pointer;
+        width:120px;
+        height:40px;
+        line-height: 40px;
+        border-radius: 4px;
+        outline: none;
+        border: none;
+        background-color: rgb(246, 103, 103);
+        box-shadow: 0 1px 2px 1px  rgba(0, 0, 0, .19);
+        color: white;
+        font-weight: bold;
+        z-index: 999;
+    }
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity 0.4s;
+    }
+    .fade-enter, .fade-leave-to {
+        opacity: 0;
+    }
+</style>
